3cd58d6cef584e14af988dd9f131e1399f46a319
crwells86 <calebrwells@gmail.com>
2024-10-03 15:47:44 -0700

    First commit for RPS game

---
+++ RPS/RPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPSApp.swift
+//  RPS
+//
+//  Created by Caleb Wells on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct RPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
